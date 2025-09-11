8cbed1e0236e9f3c06686ffef0267bab482c14b3
Bruno <brunosouza141@gmail.com>
2025-09-11 01:02:37 -0300

    1

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,10 @@
 const hamburger = document.querySelector("#hamburger");
 const list = document.querySelector(".nav-list");
+const templeh1 = document.querySelector("h1");
 
 hamburger.addEventListener("click",() => {
     hamburger.classList.toggle("open");
     list.classList.toggle("open");
+    templeh1.style.alignSelf = "flex-start";
+    templeh1.style.paddingTop = "10px";  
 });
